b60167b1e198a328894e127c8b19f9cf6f061a3b
Robert Gleim <rgleim@outlook.com>
2025-10-22 14:46:16 -0500

    react started cleaned out and routes added

---
+++ Grace_Lutheran/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+
+
+
+function App() {
+  
+
+  return (
+   <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+   </>
+  )
+}
+
+export default App
