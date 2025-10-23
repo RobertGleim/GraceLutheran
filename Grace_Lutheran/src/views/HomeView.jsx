60ed4ef99e33b58787df44aaebeb66e0a9144c22
Robert Gleim <rgleim@outlook.com>
2025-10-23 14:08:50 -0500

    auth and login added and fixed

---
+++ Grace_Lutheran/src/views/HomeView.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './HomeView.css'
+
+
+const HomeView = () => {
+  return (
+    <>
+      <div className="home-view">
+        <h1>Welcome to Grace Lutheran Church</h1>
+        <p>Your faith journey begins here.</p>
+      </div>
+      <div className="daily-verse">
+        <h2>Daily verse</h2>
+      </div>
+    </>
+  )
+}
+
+export default HomeView
