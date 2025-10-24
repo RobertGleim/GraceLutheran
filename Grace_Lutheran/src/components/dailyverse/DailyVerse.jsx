cf772e00c3bc8ab89ba308bd8b57ecf2c8897f65
Robert Gleim <rgleim@outlook.com>
2025-10-24 14:45:12 -0500

    cards added on homeview

--- Grace_Lutheran/src/components/dailyverse/DailyVerse.jsx
+++ Grace_Lutheran/src/components/dailyverse/DailyVerse.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './DailyVerse.css';
-import { API_KEY } from '../../my_key.jsx';
+import { API_KEY } from '../../../my_key.jsx';
 
 
 const BIBLE_ID = '06125adad2d5898a-01'; // The Holy Bible, American Standard Version
---
+++ Grace_Lutheran/src/views/ChurchView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChurchView = () => {
+  return (
+    <div>ChurchView</div>
+  )
+}
+
+export default ChurchView
